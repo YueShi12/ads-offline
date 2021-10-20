6682b39d5ed428b290bfd82a3580ea2ee98b5ea5
YueShi12 <92110708+YueShi12@users.noreply.github.com>
2021-10-20 20:13:52 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -3,11 +3,8 @@ class Patient:
     # initiliaze a patient by storing the variables given by the input
     def __init__(self, startI1, endI1, delay, lengthI2):
         # store it in lists, with first element always None to ease indexing
-        self.startIs = [None, startI1, None]
-        self.endIs = [None, endI1, None]
+        self.startIs =  startI1
+        self.endIs =  endI1
         self.delay = delay
         self.lengthI2 = lengthI2
 
-        self.slots = [None, None, None]
-
-        self.locations = [None, None, None]
